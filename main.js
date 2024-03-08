58f4bad4b3a379cbc9439c9f4e689e023182e30d
dev.magi <azmgmohamed5@gmail.com>
2024-03-08 02:06:05 +0100

    files added

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+  strings: ["Découvrez des produits uniques et élégants pour votre style de vie."],
+  typeSpeed: 40,
+  backSpeed: 20,
+  cursorChar: '',
+  backDelay: 500,
+  loop: true,
+});
